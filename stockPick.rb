e9891e03fe12f604d10f33646d5212585dd5136d
Kevin Le <platn02@gmail.com>
2017-12-01 16:21:22 -0800

    Description Added
    =begin
       Given an unsorted array of stock prices, pick the best time to buy and sell.
    
       Idea: Go through the array from beginning to end, check each of the indexes,
       do a difference of the current index versus each index after for each to get
       the profit value, update the maximum value each time and then store those two
       indices, this will take an O(N^2) time with an O(1) space. 
    =end

--- stockPick.rb
+++ stockPick.rb
@@ -4,7 +4,7 @@
    Idea: Go through the array from beginning to end, check each of the indexes,
    do a difference of the current index versus each index after for each to get
    the profit value, update the maximum value each time and then store those two
-   indices, this will take an O(N^2) time with an O(1) space.
+   indices, this will take an O(N^2) time with an O(1) space. 
 =end
 
 def stockPick(stocks)
