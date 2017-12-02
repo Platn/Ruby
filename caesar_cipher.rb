93b4c5b3b79eaf4e59ef782dcd635bd15a701b65
Kevin Le <platn02@gmail.com>
2017-12-01 16:20:18 -0800

    Add files via upload
    Shift each letter to the right, rotate back around for overflow.
       If it is a symbol, leave it alone. Convert ascii to integer
       and integer to ascii

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,9 @@
+=begin
+   Shift each letter to the right, rotate back around for overflow.
+   If it is a symbol, leave it alone. Convert ascii to integer
+   and integer to ascii
+=end
+
 def caesar_cipher(passed_string, shift)
    passed_string.each_char do |value| 
          valueOrd = value.ord
